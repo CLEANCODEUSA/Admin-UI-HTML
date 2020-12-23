47c9429f1ca9f0d1ff07583285ea958df76c1916
PHPRHiNOS <vanbeek@drupalsailor.com>
2020-12-23 13:50:18 -0500

    Create create-new-event-modal-tags-select2.js

---
+++ client-ui/assets/js/create-new-event-modal-tags-select2.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function () {
+    jQuery("#tag-selector").select2({
+        maximumSelectionLength: 2,
+    });
+});
+
+$(document).ready(function() {
+    $('.js-example-basic-single').select2();
+});
