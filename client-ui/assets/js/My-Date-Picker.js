503e51af8c2180f9dc0417f36029d17d6be656ec
PHPRHiNOS <vanbeek@drupalsailor.com>
2020-12-23 13:50:26 -0500

    Create My-Date-Picker.js

---
+++ client-ui/assets/js/My-Date-Picker.js
@@ -0,0 +1,8 @@
+$(function () {
+  $('#datetimepicker1, #datetimepicker2').datepicker({
+      format: "dd/mm/yyyy",
+      language: "en",
+      autoclose: true,
+      todayHighlight: true
+  });
+});
